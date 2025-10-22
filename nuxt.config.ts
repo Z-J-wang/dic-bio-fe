0eca1253eca547afe40fa4519541c8ca0480c2f2
Jay <Jay040030@outlook.com>
2025-10-22 15:48:49 +0800

    feat(nuxt): 添加 Tailwind CSS 模块并配置
    - 在 `nuxt.config.ts` 中添加 `@nuxtjs/tailwindcss` 模块
    - 调整模块声明顺序以保持代码风格一致
    - 在 `package.json` 中添加 `@nuxtjs/tailwindcss` 依赖项
    - 新增 `tailwind.config.js` 配置文件，禁用全局样式重置（preflight）
    - 配置 Tailwind 内容扫描路径以支持项目结构

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/test-utils'
+    '@nuxt/test-utils',
+    '@nuxtjs/tailwindcss',
   ],
+  devtools: { enabled: true },
+  compatibilityDate: '2025-07-15',
   eslint: {
     config: {
-      stylistic: true // 开启 eslint-stylistic
-    }
-  }
+      stylistic: true, // 开启 eslint-stylistic
+    },
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  corePlugins: {
+    preflight: false, // 禁用全局样式重置，以避免样式冲突。开启此选项会取消基础样式的引入。本项目仅使用工具类样式和组件类样式，故而禁用全局样式重置。
+  },
+  prefix: '',
+  content: ['./index.html', './app/**/*.{vue,js,ts,jsx,tsx}'], // tailwind CSS 生效路径
+  theme: {
+    extend: {
+      colors: {
+        // primary: '#409EFF',
+      },
+    },
+  },
+  plugins: [],
+}
