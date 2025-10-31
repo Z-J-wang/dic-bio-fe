c822ddb2335e0b235a7fbdb84f2b2d7d776e3c01
zhijie-wang <jay040030@outlook.com>
2025-10-31 14:43:46 +0800

    feat(footer): 重构页脚组件并增强国际化支持
    - 添加 `.stylelintignore` 配置，忽略构建输出、依赖、日志等无关文件
    - 新增 `app.config.ts`，配置 UI 库中页脚的默认样式
    - 重构 `BaseFooter.vue` 组件：
      - 引入多语言支持（i18n）
      - 动态渲染页脚栏目及链接
      - 更新品牌名称为 "SMART-NBC"
      - 添加国际化词条支持中英文
    - 更新 `en.json` 和 `zh-CN.json`，补充页脚相关文案翻译

---
+++ app/app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+  ui: {
+    footer: {
+      slots: {
+        root: 'bg-black text-white py-8',
+        bottom: 'p-0!',
+      },
+    },
+  },
+})
--- app/components/BaseFooter.vue
+++ app/components/BaseFooter.vue
@@ -1,20 +1,46 @@
 <script setup lang="ts">
+import type { FooterColumn } from '@nuxt/ui'
+
+const { t } = useI18n()
 const year = new Date().getFullYear()
+const columns = computed<FooterColumn[]>(() => {
+  return [
+    {
+      label: t('about'),
+      children: [
+        { label: t('aboutUs'), to: { path: '/about' } },
+        { label: t('contactUs'), to: { path: '/about' } },
+        { label: t('news'), to: { path: '/about' } },
+      ],
+    },
+    {
+      label: t('support&docs'),
+      children: [
+        { label: t('help'), to: { path: '/' } },
+        { label: t('FAQ'), to: { path: '/' } },
+        { label: t('userManual'), to: { path: '/' } },
+      ],
+    },
+  ]
+})
 </script>
 
 <template>
   <UFooter>
     <template #left>
-      <div>CNG</div>
+      <div class=" text-3xl font-extrabold">
+        SMART-NBC
+      </div>
     </template>
-
+    <UFooterColumns :columns="columns" />
     <template #bottom>
+      <USeparator class="my-8" />
       <UContainer>
         <div class="flex justify-between items-center">
           <div>
             <i18n-t keypath="copyrighted">
               <span>
-                © {{ year }} NBC
+                © {{ year }} SMART-NBC
               </span>
             </i18n-t>
           </div>
@@ -24,3 +50,20 @@ const year = new Date().getFullYear()
     </template>
   </UFooter>
 </template>
+
+<i18n lang="json">
+{
+  "en": {
+    "support&docs": "Support & Docs",
+    "help": "Data Policy",
+    "FAQ": "FAQ",
+    "userManual": "User Manual"
+  },
+  "zh-CN": {
+    "support&docs": "帮助 & 文档",
+    "help": "数据政策",
+    "FAQ": "常见问题",
+    "userManual": "用户手册"
+  }
+}
+</i18n>
